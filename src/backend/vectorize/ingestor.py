c66e34856b6ce25e3fa620535074c69aa879b4b6
Jack Young <jdydev2003@gmail.com>
2025-09-12 15:05:32 -0700

    batch insert

--- src/backend/vectorize/ingestor.py
+++ src/backend/vectorize/ingestor.py
@@ -4,7 +4,6 @@ import chromadb  # type: ignore
 
 class Ingestor:
     def __init__(self) -> None:
-        # get env vars here?
         self.MAX_BUFFER_SIZE = 100
         self.buffer = []
 
@@ -19,7 +18,33 @@ class Ingestor:
         return collection
 
     def process_chunk(self, chunk, collection):
-        pass
+        self.buffer.append(chunk)
+        if len(self.buffer) > self.MAX_BUFFER_SIZE:
+            self.flush_buffer(collection)
+
+    def get_parameters(self):
+        ids = [chunk['id'] for chunk in self.buffer]
+        docs = [chunk['document'] for chunk in self.buffer]
+        metas = [chunk['metadata'] for chunk in self.buffer]
+        embeddings = [chunk['embedding'] for chunk in self.buffer]
+
+        parameters = {
+            'ids': ids,
+            'docs': docs,
+            'metas': metas,
+            'embeddings': embeddings
+        }
+        return parameters
 
     def flush_buffer(self, collection):
-        pass
+        parameters = self.get_parameters()
+        self.batch_insert(collection, parameters)
+        self.buffer = []
+
+    def batch_insert(self, collection, parameters):
+        collection.add(
+            ids=parameters['ids'],
+            documents=parameters['docs'],
+            metadatas=parameters['metas'],
+            embeddings=parameters['embeddings']
+        )
