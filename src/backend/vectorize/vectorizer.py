0bdc46113d787d91c1e644046bbc5bc8a93c3be6
Jack Young <jdydev2003@gmail.com>
2025-09-12 15:32:59 -0700

    simple vectorizer class

---
+++ src/backend/vectorize/vectorizer.py
@@ -0,0 +1,31 @@
+from ingestor import Ingestor
+from sentence_transformers import SentenceTransformer  # type: ignore
+
+
+class Vectorizer:
+    def __init__(self, ingestor: Ingestor) -> None:
+        self.ingestor: Ingestor = ingestor
+        pass
+
+    def vectorize_and_insert(self, chunks):
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+        client = self.ingestor.get_chroma_client()
+        collection = self.ingestor.get_chroma_collection(client)
+
+        for chunk in chunks:
+            embedded_chunk = self.vectorize_chunk(chunk, model)
+            self.ingestor.process_chunk(embedded_chunk, collection)
+
+        # flush anything that might still be in the buffer
+        self.ingestor.flush_buffer(collection)
+
+    def vectorize_chunk(self, chunk, model):
+        embedding = self.embed_text(chunk['document'], model)
+        chunk['embedding'] = embedding
+        return chunk
+
+    def embed_text(self, text, model):
+        embedding_raw = model.encode(text)
+        embedding_raw = embedding_raw.astype('float32')
+        embedding = embedding_raw.tolist()
+        return embedding
