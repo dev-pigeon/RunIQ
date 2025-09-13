7366fcd3824d2f12a7a721ea50c3c921b02ee416
Jack Young <jdydev2003@gmail.com>
2025-09-13 16:17:38 -0700

    can retrieve chunks from query

--- src/backend/rag/retriever.py
+++ src/backend/rag/retriever.py
@@ -1,6 +1,7 @@
 # takes raw query, embeds it, uses the chroma db collection to get relevant chunks
 # returns relevant chunks
 import os
+import argparse
 import chromadb  # type: ignore
 from sentence_transformers import SentenceTransformer  # type: ignore
 from vectorize.vectorizer import Vectorizer
@@ -21,5 +22,24 @@ class Retriever:
         collection = client.get_or_create_collection(name=collection_name)
         return collection
 
-    def retrieve(self, text):
-        pass
+    def retrieve(self, query_text):
+        client = self.get_chroma_client()
+        collection = self.get_chroma_collection(client)
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+        query_embedding = self.vectorizer.embed_text(query_text, model)
+        result = collection.query(
+            query_embeddings=[query_embedding],
+            n_results=3,
+        )
+
+        return result['documents']
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-q", "--query", required=True)
+    args = parser.parse_args()
+    query = args.query
+    retriever = Retriever()
+    retriever.retrieve(query)
