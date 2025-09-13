3aede9eaba6d8ad2c3137e7923f96b734836c7b0
Jack Young <jdydev2003@gmail.com>
2025-09-13 16:26:36 -0700

    util file

---
+++ src/backend/util/db.py
@@ -0,0 +1,14 @@
+import chromadb  # type: ignore
+import os
+
+
+def get_chroma_client(self):
+    perisitent_chroma_path = os.environ['RUNBOT_CHROMA_PATH']
+    client = chromadb.PersistentClient(path=perisitent_chroma_path)
+    return client
+
+
+def get_chroma_collection(self, client):
+    collection_name = os.environ['RUNBOT_CHROMA_COLLECTION']
+    collection = client.get_or_create_collection(name=collection_name)
+    return collection
