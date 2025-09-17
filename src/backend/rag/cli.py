d1d7064acb78c33cf242925ce04a2cb0ea7cc9d9
Jack Young <jdydev2003@gmail.com>
2025-09-17 14:40:31 -0700

    rag cli logging

--- src/backend/rag/cli.py
+++ src/backend/rag/cli.py
@@ -2,6 +2,16 @@ import os
 import json
 from rag.retriever import Retriever
 from rag.generator import Generator
+import logging
+
+
+# set up logger
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="rag/rag.log",
+                    format="%(asctime)s [%(levelname)s] %(name)s %(message)s")
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("transformers").setLevel(logging.WARNING)
+logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
 
 
 def is_quit(query):
@@ -9,6 +19,7 @@ def is_quit(query):
 
 
 if __name__ == "__main__":
+    logger.info("Initializing resources.")
     prompt_config_path = os.environ['RUNBOT_PROMPT_PATH']
     with open(prompt_config_path, 'r') as file:
         config_prompt = json.load(file)['prompt']
@@ -16,9 +27,12 @@ if __name__ == "__main__":
     retriever = Retriever()
     generator = Generator()
 
+    logger.info("Beginning query sequence.")
+
     while True:
         query_text = input("Enter a query: ")
         if is_quit(query_text):
+            logger.info("Ending query sequence")
             print("Exiting...")
             break
         context = retriever.retrieve(query_text)
