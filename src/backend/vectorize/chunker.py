ec05270400dc1cad88598055189e3210a1e144b6
Jack Young <jdydev2003@gmail.com>
2025-09-18 14:18:44 -0700

    can do hybrid chunking

--- src/backend/vectorize/chunker.py
+++ src/backend/vectorize/chunker.py
@@ -1,6 +1,7 @@
 import json
 import argparse
 import logging
+import re
 
 
 class Chunker:
@@ -9,6 +10,8 @@ class Chunker:
         self.MAX_CHUNK_SIZE = chunk_size  # tokens
         self.CHUNK_OVERLAP = int(chunk_size * (chunk_overlap_percent / 100))
         self.strategy = chunking_strategy
+        self.ABBREVIATIONS = {"Mr.", "Mrs.", "Dr.",
+                              "Prof.", "Sr.", "Jr.", "e.g.", "i.e."}
 
     def chunk_file(self, data):
         # data is json file containing a parsed document
@@ -45,17 +48,37 @@ class Chunker:
     def tokenize(self, text):
         return text.split()
 
+    def get_sentences(self, paragraph):
+        sentences = []
+        left, right = 0, 0
+        while right < len(paragraph):
+            if self.is_sentence_boundary(right, paragraph):
+                sentence = paragraph[left: right + 1].strip()
+                sentences.append(sentence)
+                left = right + 1
+
+            right += 1
+        return sentences
+
+    def is_sentence_boundary(self, index, paragraph):
+        if paragraph[index] in ".!?":
+            start = paragraph.rfind(" ", 0, index) + 1
+            prev_word = paragraph[start:index+1]
+            if prev_word in self.ABBREVIATIONS:
+                return False
+            else:
+                return True
+        return False
+
     def emit_chunks(self, chunks, curr_chunk, source):
         chunk_text = " ".join(curr_chunk)
         chunk = self.make_chunk(
             chunk_text, len(chunks), source)
         chunks.append(chunk)
-        curr_chunk = curr_chunk[-self.CHUNK_OVERLAP:]
-        return curr_chunk
 
     def chunk_strategy_naive(self, data):
         # builds each chunk at the token level
-        # ensures chunks are all the same size
+        # overlap is token wise
         chunks = []
         curr_chunk = []
         source = data['source']
@@ -65,14 +88,15 @@ class Chunker:
                 if len(curr_chunk) < self.MAX_CHUNK_SIZE:
                     curr_chunk.append(token)
                 else:
-                    curr_chunk = self.emit_chunks(chunks, curr_chunk, source)
+                    self.emit_chunks(chunks, curr_chunk, source)
+                    curr_chunk = curr_chunk[-self.CHUNK_OVERLAP:]
 
-        curr_chunk = self.emit_chunks(chunks, curr_chunk, source)
+        self.emit_chunks(chunks, curr_chunk, source)
         return chunks
 
     def chunk_strategy_paragraph(self, data):
         # builds each chunk at the paragraph level
-        # helps ensure chunks capture semantic meaning
+        # overlap is token wise
         chunks = []
         curr_chunk = []
         source = data['source']
@@ -80,10 +104,29 @@ class Chunker:
             tokens = self.tokenize(para)
             # decide on token
             if len(curr_chunk) + len(tokens) > self.MAX_CHUNK_SIZE:
-                curr_chunk = self.emit_chunks(chunks, curr_chunk, source)
+                self.emit_chunks(chunks, curr_chunk, source)
+                curr_chunk = curr_chunk[-self.CHUNK_OVERLAP:]
             else:
                 curr_chunk.extend(tokens)
-        curr_chunk = self.emit_chunks(chunks, curr_chunk, source)
+        self.emit_chunks(chunks, curr_chunk, source)
+        return chunks
+
+    def chunk_strategy_hybrid(self, data):
+        # builds a sentence level
+        # overlap is prev sentence, not token wise
+        chunks = []
+        curr_chunk = []
+        source = data['source']
+        for para in data['paragraphs']:
+            sentences = self.get_sentences(para)
+            for sentence in sentences:
+                sentence_tokens = self.tokenize(sentence)
+                if len(curr_chunk) + len(sentence_tokens) > self.MAX_CHUNK_SIZE:
+                    self.emit_chunks(chunks, curr_chunk, source)
+                    curr_chunk = sentence_tokens.copy()
+                else:
+                    curr_chunk.extend(sentence_tokens)
+        self.emit_chunks(chunks, curr_chunk, source)
         return chunks
 
     def chunk_paragraphs(self, data):
@@ -96,7 +139,10 @@ class Chunker:
                     chunks = self.chunk_strategy_naive(data)
                 case "paragraph":
                     chunks = self.chunk_strategy_paragraph(data)
+                case "hybrid":
+                    chunks = self.chunk_strategy_hybrid(data)
 
+            print(chunks)
             return chunks
         else:
             self.logger.debug(f"{data['source']} has no paragraphs.")
@@ -127,5 +173,5 @@ if __name__ == "__main__":
     with open(data_path, 'r') as file:
         data = json.load(file)
 
-    chunker = Chunker(chunking_strategy="paragraph")
+    chunker = Chunker(chunking_strategy="hybrid")
     chunks = chunker.chunk_file(data)
