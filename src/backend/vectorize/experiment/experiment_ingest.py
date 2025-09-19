d6d5e8d8ed2eb97d8e54376613cd8e541fb6de49
Jack Young <jdydev2003@gmail.com>
2025-09-19 11:20:49 -0700

    implemented experiment ingest

---
+++ src/backend/vectorize/experiment/experiment_ingest.py
@@ -0,0 +1,60 @@
+from vectorize.chunker import Chunker
+from vectorize.vectorizer import Vectorizer
+from vectorize.ingestor import Ingestor
+import json
+import logging
+
+
+def open_json(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def run_experiment_ingest_pipeline():
+    logger.info(
+        f"Beginning test experiment ingestion sequence for {output_collection}")
+    for path in chunker_paths:
+        data = open_json(path)
+        chunks = chunker.chunk_file(data)
+        vectorizer.embed_and_insert(chunks, ingestor)
+    logger.info(f"finished chunking and ingesting for {output_collection}")
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename="vectorize/experiment/experiment.log",
+                        level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("transformers").setLevel(logging.WARNING)
+    logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
+
+    config_path = "./dev/config/experiment/config.json"
+    config = open_json(config_path)
+
+    chunker_config = open_json(config['chunker_config_path'])
+    chunker_paths = chunker_config['paths']
+
+    ingestor = Ingestor()
+
+    # everyhing below is in the loop
+    for model in config['models']:
+        for strategy in config['chunk_strategies']:
+            for chunk_size in config['chunk_sizes']:
+                # if its not hybrid do the overlaps, else just call it with the sizes and move on
+                if strategy != "hybrid":
+                    for overlap in config['chunk_overlaps']:
+                        output_collection = f"MODEL{model}-TYPE{strategy}-CHUNKS{chunk_size}-OVERLAP{overlap}"
+                        chunker = Chunker(
+                            chunk_size=chunk_size, chunk_overlap_percent=overlap, chunking_strategy=strategy)
+                        vectorizer = Vectorizer(
+                            model_type=model, collection_name=output_collection)
+                        run_experiment_ingest_pipeline()
+
+                else:
+                    output_collection = f"MODEL{model}-TYPE{strategy}-CHUNKS{chunk_size}-OVERLAPnone"
+                    chunker = Chunker(
+                        chunk_size=chunk_size, chunking_strategy=strategy)
+                    vectorizer = Vectorizer(
+                        model_type=model, collection_name=output_collection)
+                    run_experiment_ingest_pipeline()
