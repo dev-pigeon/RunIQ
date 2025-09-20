f723937a4f4afaf0b895e229a739131084ee6a8d
Jack Young <jdydev2003@gmail.com>
2025-09-20 14:58:47 -0700

    can run the queries for each collection

--- src/backend/vectorize/experiment/validate_retrieval.py
+++ src/backend/vectorize/experiment/validate_retrieval.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from rag.retriever import Retriever
+from vectorize.vectorizer import Vectorizer
 from sentence_transformers import SentenceTransformer  # type: ignore
 
 
@@ -10,20 +11,14 @@ def open_json(file_path):
     return data
 
 
-def get_collection_names(config):
-    collection_names = []
-    for model in config['models']:
-        for strategy in config['chunk_strategies']:
-            for chunk_size in config['chunk_sizes']:
-                # if its not hybrid do the overlaps, else just call it with the sizes and move on
-                if strategy != "hybrid":
-                    for overlap in config['chunk_overlaps']:
-                        collection_name = f"MODEL{model}-TYPE{strategy}-CHUNKS{chunk_size}-OVERLAP{overlap}"
-                        collection_names.append(collection_name)
-                else:
-                    collection_name = f"MODEL{model}-TYPE{strategy}-CHUNKS{chunk_size}-OVERLAPnone"
-                    collection_names.append(collection_name)
-    return collection_names
+def run_queries(collection_name, model, queries):
+    retriever = Retriever(collection_name=collection_name)
+    vectorizer = Vectorizer(collection_name=collection_name)
+    logger.debug(f"Running queries for {collection_name}")
+    for query in queries['queries']:
+        results = retriever.retrieve_chunks(query['text'], model)
+        # get the precision at k for that
+        # add it to the mean
 
 
 if __name__ == "__main__":
@@ -37,4 +32,18 @@ if __name__ == "__main__":
     config_path = "./dev/config/experiment/config.json"
     config = open_json(config_path)
 
-    collection_names = get_collection_names(config)
+    queries_path = "./dev/data/experiment/queries.json"
+    queries = open_json(queries_path)
+
+    for model_name in config['models']:
+        model = SentenceTransformer(model_name)
+        for strategy in config['chunk_strategies']:
+            for chunk_size in config['chunk_sizes']:
+                # if its not hybrid do the overlaps, else just call it with the sizes and move on
+                if strategy != "hybrid":
+                    for overlap in config['chunk_overlaps']:
+                        collection_name = f"MODEL{model_name}-TYPE{strategy}-CHUNKS{chunk_size}-OVERLAP{overlap}"
+                        run_queries(collection_name, model, queries)
+                else:
+                    collection_name = f"MODEL{model_name}-TYPE{strategy}-CHUNKS{chunk_size}-OVERLAPnone"
+                    run_queries(collection_name, model, queries)
