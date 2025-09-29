7552b6dd458da9db5a25cb0cef3d2fba5ab6e36f
Jack Young <jdydev2003@gmail.com>
2025-09-29 13:46:48 -0700

    simple buffer

--- src/rag/buffer.py
+++ src/rag/buffer.py
@@ -1,12 +1,20 @@
 class Buffer:
     def __init__(self, max_turns=5) -> None:
         self.max_turns = max_turns
+        self.history = []
 
     def add(self, query, response):
-        pass
+        entry = self.format_entry(query, response)
+        if len(self.history) > self.max_turns:
+            self.history.pop()
+        self.history.append(entry)
 
     def format_entry(self, query, response):
-        pass
+        entry = f"[USER:]\n{query}\n[RESPONSE:]{response}"
+        return entry
 
     def to_string(self):
-        pass
+        history_string = ""
+        for entry in self.history:
+            history_string += entry
+        return history_string
