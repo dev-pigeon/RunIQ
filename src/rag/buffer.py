9908627d23f19521f38382dae219ef03e0afd9de
Jack Young <jdydev2003@gmail.com>
2025-09-29 13:41:14 -0700

    buffer skeleton

---
+++ src/rag/buffer.py
@@ -0,0 +1,12 @@
+class Buffer:
+    def __init__(self, max_turns=5) -> None:
+        self.max_turns = max_turns
+
+    def add(self, query, response):
+        pass
+
+    def format_entry(self, query, response):
+        pass
+
+    def to_string(self):
+        pass
